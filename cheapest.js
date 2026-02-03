39b2cb2f4a9648a003e1a2bef5a8e98dd4d8a79e
programmermosharof <mosharof.dev@gmail.com>
2026-02-03 13:55:41 +0600

    topic 4 complete

---
+++ cheapest.js
@@ -0,0 +1,18 @@
+const mobile =[
+    { name: "Sumsung", price: 15000 },
+    { name: "Vivo", price: 5000 },
+    { name: "Iphone", price: 150000 },
+    { name: "Redmi", price: 18000 },
+];
+
+function cheapestPhone(phones){
+    let min = phones[0];
+    for(let phone of phones){
+        if(phone.price > min.price){
+            min = phone;
+        }
+    }
+    return min;
+}
+const cheapest = cheapestPhone(mobile);
+console.log("Chepeast Phone", cheapest);
